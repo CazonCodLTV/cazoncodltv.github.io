f8d3476529b2c269f8d53e8888debd0fb42adabc
Cazon Carter <cazon753@gmail.com>
2025-02-26 02:16:09 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cursor = document.createElement("div");
+    cursor.classList.add("cursor");
+    document.body.appendChild(cursor);
+
+    const cursorTrail = document.createElement("div");
+    cursorTrail.classList.add("cursor-trail");
+    document.body.appendChild(cursorTrail);
+
+    document.addEventListener("mousemove", (e) => {
+        cursor.style.left = `${e.clientX}px`;
+        cursor.style.top = `${e.clientY}px`;
+
+        cursorTrail.style.left = `${e.clientX}px`;
+        cursorTrail.style.top = `${e.clientY}px`;
+    });
+
+    document.addEventListener("mousedown", () => {
+        cursor.style.transform = "scale(0.8)";
+        cursorTrail.style.transform = "scale(1.2)";
+    });
+
+    document.addEventListener("mouseup", () => {
+        cursor.style.transform = "scale(1)";
+        cursorTrail.style.transform = "scale(1)";
+    });
+});
