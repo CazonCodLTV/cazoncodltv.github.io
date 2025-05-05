5c24b1b56a7d73987912510e8669a8458f9a3253
Cazon Carter <cazon753@gmail.com>
2025-05-05 01:45:02 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Function to toggle the menu
-function toggleMenu() {
-    const menu = document.querySelector('.nav-links');
-    menu.classList.toggle('active');
-}
-// transition effect pages
-window.addEventListener('load', function () {
-    document.body.classList.add('loaded');
-});
